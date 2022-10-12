2d42ffe97450ec04937ac8c2b92bec904d646417
Lucas Pereira da Silva <pslucasps@gmail.com>
2022-10-12 17:12:04 -0300

    coloca e tira fichas do tabuleiro

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ def mouse_click(button, linha, coluna):
 tabuleiro = Board(10, 10)
 tabuleiro.on_mouse_click = mouse_click
 tabuleiro.show()
+
+def colocar_ficha(ficha):
+	ficha.alternar_posicao()
+	tabuleiro[ficha.linha][ficha.coluna] += str(ficha.id)
+
+def remover_ficha(ficha):
+	tabuleiro[ficha.linha][ficha.coluna] = ""
+
+ficha1 = Ficha(1)
+colocar_ficha(ficha1)
