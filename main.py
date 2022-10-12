5edfe8a99a65ea3a6dec2af78e9862d0d941d3f6
Lucas Pereira da Silva <pslucasps@gmail.com>
2022-10-12 17:39:35 -0300

    usa temporizador do tabuleiro

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from game2dboard import Board
 
 from ficha import Ficha
 
-
 ficha_1 = Ficha(1)
 
+tabuleiro = Board(10, 10)
+
+def iniciar_tabuleiro():
+	for linha in range(0, 10):
+		for coluna in range(0, 10):
+			tabuleiro[linha][coluna] = ""
+
 def mouse_click(button, linha, coluna):
-	remover_ficha(ficha_1)
-	colocar_ficha(ficha_1)
 	print("{0}:{1}".format(linha, coluna))
 
-tabuleiro = Board(10, 10)
-tabuleiro.on_mouse_click = mouse_click
-
-for linha in range(0, 10):
-	for coluna in range(0, 10):
-		tabuleiro[linha][coluna] = ""
+def executar_rodada():
+	remover_ficha(ficha_1)
+	colocar_ficha(ficha_1)
 
 def colocar_ficha(ficha):
 	ficha.trocar_posicao()
@@ -24,6 +25,12 @@ def colocar_ficha(ficha):
 def remover_ficha(ficha):
 	tabuleiro[ficha.linha][ficha.coluna] = ""
 
-colocar_ficha(ficha_1)
+def iniciar():
+	iniciar_tabuleiro()
+	executar_rodada()
+	tabuleiro.on_mouse_click = mouse_click
+	tabuleiro.on_timer = executar_rodada
+	tabuleiro.start_timer(2 * 1000)
+	tabuleiro.show()
 
-tabuleiro.show()
+iniciar()
