c3bbf124e19e0bb92f11cfaa0d5379d40ce91bae
gustavo <gustavodiagramador@gmail.com>
2022-10-12 17:12:08 -0300

    ficha

---
+++ ficha.py
@@ -0,0 +1,10 @@
+class Ficha:
+    def __init__(self, numero):
+        id = numero
+        linha = 0
+        coluna = 0
+
+
+    def trocar_posicao(self):
+        self.linha = rand(0,9)
+        self.coluna = rand(0,9)
